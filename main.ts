c892b0aa6973b218ff8ba62a70e389c6341760dc
Brady Hurlburt <brady.hurlburt@rovercode.com>
2020-04-02 16:05:57 -0400

    Add dummy sensor polling loop

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-let value = "";
+let value = 0;
 let command = "";
 input.onButtonPressed(Button.A, function() {
   bluetooth.uartWriteString("left-sensor:1");
@@ -7,7 +7,12 @@ input.onButtonPressed(Button.B, function() {
   bluetooth.uartWriteString("left-sensor:0");
 });
 bluetooth.onBluetoothConnected(function() {
-  basic.showIcon(IconNames.Yes);
+//   basic.showIcon(IconNames.Yes);
+  while (true) {
+    basic.showIcon(IconNames.Happy);
+    bluetooth.uartWriteString("sensor-state:0,0");
+    basic.pause(2000);
+  }
 });
 input.onGesture(Gesture.Shake, function() {
   bluetooth.uartWriteString("S");
@@ -18,15 +23,19 @@ bluetooth.onBluetoothDisconnected(function() {
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function() {
   command = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Colon));
   if ("left-motor" == command) {
-    value = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine));
-    gigglebot.motorPowerAssign(gigglebotWhichMotor.Left, parseFloat(value));
-    basic.showString("L:" + value);
+    value = parseFloat(
+      bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    );
+    gigglebot.motorPowerAssign(gigglebotWhichMotor.Left, value);
+    led.plotBrightness(0, 0, (value * 255) / 100);
   } else if ("right-motor" == command) {
-    value = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine));
-    gigglebot.motorPowerAssign(gigglebotWhichMotor.Right, parseFloat(value));
-    basic.showString("R:" + value);
+    value = parseFloat(
+      bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    );
+    gigglebot.motorPowerAssign(gigglebotWhichMotor.Right, value);
+    led.plotBrightness(0, 0, (value * 255) / 100);
   }
   basic.showIcon(IconNames.Yes);
 });
 bluetooth.startUartService();
-basic.showIcon(IconNames.SmallSquare);
+basic.showString("R")
