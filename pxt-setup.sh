b54cb5536c2d328b328a667e12ec91cee4e0cc55
Brady L. Hurlburt <brady.hurlburt@rovercode.com>
2020-04-23 17:25:44 -0400

    Use variable in pxt setup script

--- pxt-setup.sh
+++ pxt-setup.sh
@@ -1,3 +1,7 @@
-node_modules/pxt/pxt target microbit@2.3.9
-echo "{\"targetdir\": \"pxt-microbit\"}" > node_modules/pxtcli.json
+# A script to allow pxt setup of a certain version of a target
+
+TARGET=$1
+VERSION=$2
+node_modules/pxt/pxt target $TARGET@$VERSION
+echo "{\"targetdir\": \"pxt-$TARGET\"}" > node_modules/pxtcli.json
 node_modules/pxt/pxt install
