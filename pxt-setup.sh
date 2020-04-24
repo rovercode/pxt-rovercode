98d583a3a1fc7754f63f2c65b63ccb777d6ee1b5
Brady L. Hurlburt <brady.hurlburt@rovercode.com>
2020-04-24 14:59:31 -0400

    Stop using pxt target

--- pxt-setup.sh
+++ pxt-setup.sh
@@ -1,7 +1,5 @@
-# A script to allow pxt setup of a certain version of a target
+# Use this instead of `pxt target` to keep it from updating our pxt-microbit version
 
 TARGET=$1
-VERSION=$2
-node_modules/pxt/pxt target $TARGET@$VERSION
 echo "{\"targetdir\": \"pxt-$TARGET\"}" > node_modules/pxtcli.json
 node_modules/pxt/pxt install
